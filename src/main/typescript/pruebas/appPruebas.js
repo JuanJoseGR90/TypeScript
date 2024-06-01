a2c1928d1f05b25af70b5b3b3cd33f1e4a165a50
hellrider1990 <juanjoseguiradoruiz90@gmail.com>
2024-06-01 14:16:47 +0200

    practicando con variables

---
+++ src/main/typescript/pruebas/appPruebas.js
@@ -0,0 +1,8 @@
+var tipoAny;
+tipoAny = 4;
+var n = 10;
+var alias = 'Hellrider1990';
+var emailPart1 = "hellrider1990";
+var emailPart2 = "@hotmail.com";
+var emailComplete = "".concat(emailPart1).concat(emailPart2);
+console.log(emailComplete);
---
+++ src/main/typescript/pruebas/appPruebas.ts
@@ -0,0 +1,15 @@
+let tipoAny
+
+tipoAny = 4;
+
+let n: number = 10;
+
+let alias: string = 'Hellrider1990';
+
+let emailPart1: string = `hellrider1990`;
+
+let emailPart2: string = `@hotmail.com`;
+
+let emailComplete: string = `${emailPart1}${emailPart2}`;
+
+console.log(emailComplete);
